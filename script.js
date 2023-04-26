7390b942ec26f32d3bf39dead60899b236ea0ba9
buildwithaman <112074298+Aman2818@users.noreply.github.com>
2023-04-26 16:44:14 +0530

    gameover functionality improved , mobile optimized

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const playBoard = document.querySelector(".play-board");
 
+
 let foodX ,foodY;
 // let snakeHeadHTML;
 let snakeX=5 , snakeY =10;
@@ -7,24 +8,39 @@ let snakeBody = [];
 let velocityX=0 , velocityY=0 ;
 let gameOver = false;
 let setIntervalId;
+let score = 0;
+let scoreElement = document.querySelector(".score");
+let highScoreElement = document.querySelector(".high-score");
+
+let highScore = localStorage.getItem("high-score") || 0;
+highScoreElement.innerText = `High Score: ${highScore}`;
+let controlButtons = document.querySelectorAll(".control-buttons i");
+
+
+
+
 
 const changeSnakeDirection = (e)=>{
     //changing the direction of the snake on pressing the keys 
-    if(e.key === "ArrowUp"){
+    if(e.key === "ArrowUp" && velocityY != 1){
         velocityX= 0;
         velocityY= -1;
-    }else if(e.key === "ArrowDown"){
+    }else if(e.key === "ArrowDown" && velocityY != -1){
         velocityX = 0;
         velocityY = 1;
-    }else if(e.key === "ArrowLeft"){
+    }else if(e.key === "ArrowLeft" && velocityX != 1){
         velocityX = -1;
         velocityY = 0;
-    }else if(e.key === "ArrowRight"){
+    }else if(e.key === "ArrowRight" && velocityX != -1){
         velocityX = 1;
         velocityY = 0;
     }
     // initGame();
 }
+
+controlButtons.forEach((key) => {
+    key.addEventListener("click", () => changeSnakeDirection({key:key.dataset.arrowkeys}));
+});
 
 const handleGameOver = () => {
     clearInterval(setIntervalId);
@@ -47,6 +63,14 @@ const initGame = () => {
         snakeFoodPosition();
         snakeBody.push([foodX , foodY]);
         // console.log(snakeBody);
+        score++ ;
+        highScore = score ;
+        if(score >= highScore){
+            highScore = score;
+        }
+        localStorage.setItem("high-score" , highScore);
+        scoreElement.innerText = `Score: ${score}`;
+
     }
     for(let i=snakeBody.length-1; i>0; i--){
         snakeBody[i]=snakeBody[i-1];
@@ -62,6 +86,10 @@ const initGame = () => {
 
     for(let i=0; i<snakeBody.length; i++){
         snakeFoodHTML += `<div class="head" style="grid-area: ${snakeBody[i][1]} / ${snakeBody[i][0]} "></div>`;
+
+        if(i != 0 && snakeBody[0][1] === snakeBody[i][1] && snakeBody[0][0] && snakeBody[0][0] === snakeBody[i][0]){
+            gameOver = true;
+        }
     }
 
 
